c3d5127cc208474d745ce409877cf013944ac730
Scott Sanford <scottsanford1217@gmail.com>
2020-04-17 13:06:23 -0500

    conditionally switch toggle angle

--- src/app/workspace/sidenav/SidenavIcon.js
+++ src/app/workspace/sidenav/SidenavIcon.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const SidenavIcon = ({ handleClick }) => {
+const SidenavIcon = ({ handleClick, isOpen }) => {
+
+	const angleDirection = isOpen ? 'left' : 'right'
 
 	return (
 		<div className="sidenav__toggle" onClick={handleClick}>
-			<span className="fa fa-angle-left"></span>
+			<span className={`fa fa-angle-${angleDirection}`}></span>
 		</div>
 	)
 }
