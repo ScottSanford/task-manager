ce0c56394de6070e4d0f170e8a527f6fc21c3dcd
Scott Sanford <scottsanford1217@gmail.com>
2020-04-18 18:56:46 -0500

    toggle active state

--- src/app/workspace/sidenav/projects/Projects.js
+++ src/app/workspace/sidenav/projects/Projects.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Projects.css'
 
 const Projects = ({ projects }) => {
-	console.log('projects', projects)
+
+	const [activeProject, setActiveProject] = useState('')
+	const handleClick = id => setActiveProject(id)
+
 	return (
 		<div className="projects">
 			{projects.map(project => {
+				const selectedProject = activeProject === project.id ? 'project--selected' : ''
 				return (
-					<div key={project.id} className="projects__item">
+					<div key={project.id} className={`projects__item ${selectedProject}`} onClick={() => handleClick(project.id)}>
 						<span className={`fa fa-${project.icon}`}></span>
 						<span className="projects__item-title">{project.title}</span>
 					</div>
