cd4bcb7a0e3064878ff3644a462886213658a2a8
Scott Sanford <scottsanford1217@gmail.com>
2020-04-19 20:05:14 -0500

    convert px to rem, alphabetize

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './Header.css'
 import logo from '../../assets/logo.png'
 
 const Header = () => {
+
 	return (
 		<nav className="header">
 			<div className="header__logo"><img alt="logo" src={logo} /></div>
-			<Link to="/" className="header__nav">Workspace</Link>
-			<Link to="/dashboard" className="header__nav">Dashboard</Link>
+
+			<NavLink exact to="/" className="header__nav" activeClassName="header--selected">Workspace</NavLink>
+			<NavLink exact to="/dashboard" className="header__nav" activeClassName="header--selected">Dashboard</NavLink>
 		</nav>
 	)
 }
