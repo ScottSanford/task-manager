11923a56ea99a5c7ac3fc70fc147cccd72ffb12c
Scott Sanford <scottsanford1217@gmail.com>
2020-04-29 14:51:20 -0500

    use styled compoenents for Priority component

--- src/app/workspace/ticket/Priority.js
+++ src/app/workspace/ticket/Priority.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import './Priority.css'
+import styled from 'styled-components'
 
-const Priority = ({ level }) => {
 
-	const overrideStyles = () => {
-		switch (level) {
-			case 'low':
-				return { background: 'var(--color-green-7)' }
-			case 'medium':
-				return { background: 'var(--color-yellow-7)' }
-			default:
-				return { background: 'var(--color-red-7)' }
-		}
+const overrideStyles = (level) => {
+	switch (level) {
+		case 'low':
+			return 'var(--color-green-7)'
+		case 'medium':
+			return 'var(--color-yellow-7)'
+		default:
+			return 'var(--color-red-7)'
 	}
-
-	return <div className="ticket__tag" style={overrideStyles()}></div>
 }
+
+const PriorityWrapper = styled.div`
+	background: ${({ level }) => overrideStyles(level)};
+	grid-column: 1 / 3;
+	border-radius: var(--b-radius-sm);
+	width: 5rem;
+	height: 5px;
+`
+
+const Priority = ({ level }) => <PriorityWrapper level={level}></PriorityWrapper>
 
 export default Priority
