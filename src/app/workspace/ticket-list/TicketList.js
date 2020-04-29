baca5e08e102772436a1331e4d838de05f0b1358
Scott Sanford <scottsanford1217@gmail.com>
2020-04-28 22:55:43 -0500

    add droppable component

--- src/app/workspace/ticket-list/TicketList.js
+++ src/app/workspace/ticket-list/TicketList.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './TicketList.css'
 import Ticket from '../ticket/Ticket'
 import CardComposer from '../card-composer/CardComposer'
-
+import { Droppable } from 'react-beautiful-dnd'
 
 const TicketList = ({
 	column,
@@ -34,25 +34,30 @@ const TicketList = ({
 	}
 
 	return (
-		<div className="card-list">
-			<div className="card-list__header">
-				<div className="card-list__title">{column.title}</div>
-				<div className="fa fa-ellipsis-h" onClick={handleDeleteClick}></div>
-			</div>
-			<div className="card-list__container">
-				<div className="ticket-list__container">
-					{tickets.map((item, index) => {
-						return <Ticket
-							key={index.toString()}
-							item={item}
-							onDragRemove={(item) => handleDragRemove(item)}
-							openModal={(item) => openModal(item, id)} />
-					})}
+		<Droppable droppableId={column.id}>
+			{(provided) => (
+				<div ref={provided.innerRef} {...provided.droppableProps} className="card-list">
+					<div className="card-list__header">
+						<div className="card-list__title">{column.title}</div>
+						<div className="fa fa-ellipsis-h" onClick={handleDeleteClick}></div>
+					</div>
+					<div className="card-list__container">
+						<div className="ticket-list__container">
+							{tickets.map((item, index) => {
+								return <Ticket
+									key={index.toString()}
+									item={item}
+									onDragRemove={(item) => handleDragRemove(item)}
+									openModal={(item) => openModal(item, id)} />
+							})}
+						</div>
+						{provided.placeholder}
+						{isComposing ? <CardComposer onCardEnter={handleOnCardEnter} /> : ''}
+						<div className="card-list__add-button" onClick={handleAddActionClick}>Add Card</div>
+					</div>
 				</div>
-				{isComposing ? <CardComposer onCardEnter={handleOnCardEnter} /> : ''}
-				<div className="card-list__add-button" onClick={handleAddActionClick}>Add Card</div>
-			</div>
-		</div>
+			)}
+		</Droppable>
 	)
 }
 
